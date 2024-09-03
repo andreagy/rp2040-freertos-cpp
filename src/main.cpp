4d4a37bcb01bb2c90557937f93884ffe16f3aeb8
andreagy <andi.gyimoti@gmail.com>
2024-09-03 21:56:46 +0300

    Button code sequence properly lights up LED

--- src/main.cpp
+++ src/main.cpp
@@ -30,7 +30,7 @@ void button_read_task(void *param) {
     gpio_pull_up(button_pin);
 
     while (true) {
-        if (!gpio_get(button_pin)) {  // Button pressed
+        if (!gpio_get(button_pin)) {
             while (!gpio_get(button_pin)) {
                 vTaskDelay(pdMS_TO_TICKS(50)); // Debounce delay
             }
@@ -46,35 +46,31 @@ void process_button_task(void *param) {
     task_params *tpr = (task_params *) param;
 
     uint32_t button_value;
-
+    vector<uint32_t> code_sequence = {9, 9, 7, 8, 7}; // SW0, SW0, SW2, SW1, SW2
+    int current_index = 0;  // Track progress in the sequence
+    vector<uint32_t> buffer_sequence {0};
 
     gpio_init(20);
     gpio_set_dir(20, GPIO_OUT);
     gpio_put(20, 0);  // LED OFF
 
-    gpio_init(21);
-    gpio_set_dir(21, GPIO_OUT);
-    gpio_put(21, 0);  // LED OFF
-
-    gpio_init(22);
-    gpio_set_dir(22, GPIO_OUT);
-    gpio_put(22, 0);  // LED OFF
 
     while(1) {
-        if (xQueueReceive(tpr->comm_q, static_cast<void *>(&button_value), pdMS_TO_TICKS(500)) == pdTRUE) {
-            if (button_value == 9) {
-                gpio_put(20, 1);
-                vTaskDelay(1000);
-                gpio_put(20, 0);
-            } else if (button_value == 8) {
-                gpio_put(21, 1);
-                vTaskDelay(1000);
-                gpio_put(21, 0);
-            } else if (button_value == 7) {
-                gpio_put(22, 1);
-                vTaskDelay(1000);
-                gpio_put(22, 0);
+        if (xQueueReceive(tpr->comm_q, static_cast<void *>(&button_value), pdMS_TO_TICKS(1000)) == pdTRUE) {
+            if (button_value == code_sequence[current_index]) {
+                current_index++;
+
+                if (current_index == code_sequence.size()) {
+                    gpio_put(20, 1);
+                    vTaskDelay(pdMS_TO_TICKS(5000));
+                    gpio_put(20, 0);
+                    current_index = 0;
+                }
+            } else {
+                current_index = 0; // Incorrect button, reset sequence
             }
+        } else {
+            current_index = 0; // Timeout, reset sequence
         }
 
     }
@@ -90,7 +86,7 @@ int main()
     static task_params sw1_params = { .comm_q = button_queue, .pin = 8 };
     static task_params sw2_params = { .comm_q = button_queue, .pin = 7 };
 
-    static task_params process_param = {.comm_q = button_queue, .pin = 8};
+    static task_params process_param = {.comm_q = button_queue};
 
     vQueueAddToRegistry(button_queue, "BUTTON_Q");
     xTaskCreate(button_read_task, "BUTTON_SW0", 512, (void *) &sw0_params, tskIDLE_PRIORITY + 1, NULL);
@@ -98,7 +94,7 @@ int main()
     xTaskCreate(button_read_task, "BUTTON_SW2", 512, (void *) &sw2_params, tskIDLE_PRIORITY + 1, NULL);
 
     // Create button processing task
-    xTaskCreate(process_button_task, "PROCESS", 1024, (void *) &process_param, tskIDLE_PRIORITY + 2, NULL);
+    xTaskCreate(process_button_task, "PROCESS", 512, (void *) &process_param, tskIDLE_PRIORITY + 2, NULL);
 
     vTaskStartScheduler();
 
