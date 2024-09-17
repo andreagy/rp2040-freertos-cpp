23925c02f049769e2f8a124b5413065a4bfd3b2c
andreagy <andi.gyimoti@gmail.com>
2024-09-17 14:54:30 +0300

    button task implemented

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,8 @@ uint32_t read_runtime_ctr(void) {
 
 // Pins
 const uint SW0_PIN = 9;
-const uint LED_PIN = 22;
+const uint SW1_PIN = 8;
+const uint SW2_PIN = 7;
 
 // Event bits
 #define TASK1_BIT   (1UL << 0UL) // zero shift for bit0
@@ -29,24 +30,54 @@ struct debugEvent {
     uint32_t data[3];
 };
 
-QueueHandle_t log_q;
+QueueHandle_t syslog_q;
 EventGroupHandle_t event_group;
 
+struct Button {
+    uint pin;
+    int *counter;
+    uint32_t *last_button_press_time;
+    EventBits_t bit;
+};
+
+int SW0_count {0};
+int SW1_count {0};
+int SW2_count {0};
+
+uint32_t last_SW0_press_time {0};
+uint32_t last_SW1_press_time {0};
+uint32_t last_SW2_press_time {0};
+
+
+Button buttons[] = {
+        {SW0_PIN, &SW0_count, &last_SW0_press_time, TASK1_BIT},
+        {SW1_PIN, &SW1_count, &last_SW1_press_time, TASK1_BIT},
+        {SW2_PIN, &SW2_count, &last_SW2_press_time, TASK1_BIT}
+};
+
+
+
+void create_button(int gpio) {
+    gpio_init(gpio);
+    gpio_set_dir(gpio, GPIO_IN);
+    gpio_pull_up(gpio);
+}
+
 void debug(const char *format, uint32_t d1, uint32_t d2, uint32_t d3) {
-    debugEvent e;
+    debugEvent e{};
     e.format = format;
     e.data[0] = d1;
     e.data[1] = d2;
     e.data[2] = d3;
-    xQueueSend(log_q, &e, portMAX_DELAY);
+    xQueueSend(syslog_q, &e, portMAX_DELAY);
 }
 
 void debugTask(void *param) {
     char buffer[64];
-    debugEvent e;
+    debugEvent e{};
 
     while (1) {
-        if (xQueueReceive(log_q, &e, portMAX_DELAY) == pdTRUE) {
+        if (xQueueReceive(syslog_q, &e, portMAX_DELAY) == pdTRUE) {
             //get the current timestamp
             TickType_t timestamp = xTaskGetTickCount();
 
@@ -57,19 +88,23 @@ void debugTask(void *param) {
     }
 }
 
-void button_task(void *param) {
-    gpio_init(SW0_PIN);
-    gpio_set_dir(SW0_PIN, GPIO_IN);
-    gpio_pull_up(SW0_PIN);
+void buttonTask(void *param) {
+
+    for (auto & button : buttons) {
+        create_button(button.pin);
+    }
 
     while (true) {
-        if (!gpio_get(SW0_PIN)) {
-            while (!gpio_get(SW0_PIN)) {
-                uint32_t button_value = SW0_PIN;
-                //xQueueSendToBack(log_q, static_cast<void *>(&button_value), portMAX_DELAY);
-                xEventGroupSetBits(event_group, TASK1_BIT);
+        uint32_t current_time = to_ms_since_boot(get_absolute_time());
+        for (int i = 0; i < 3; i++) {
+            if (!gpio_get(buttons[i].pin)) {
+                if (current_time - *buttons[i].last_button_press_time >= 250) {
+                    *buttons[i].last_button_press_time = current_time;
+                    (*buttons[i].counter)++;
+                    debug("Button: %d pressed. Count: %d\n", i, *buttons[i].counter, 0);
+                    xEventGroupSetBits(event_group, buttons[i].bit);
+                }
             }
-            vTaskDelay(pdMS_TO_TICKS(50)); // Debounce delay
         }
         vTaskDelay(pdMS_TO_TICKS(10)); // Polling delay
     }
@@ -79,13 +114,15 @@ void button_task(void *param) {
 
 int main()
 {
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
+    stdio_init_all();
 
-    log_q = xQueueCreate(10, sizeof(debugEvent));
-    vQueueAddToRegistry(log_q, "LOG_Q");
+    syslog_q = xQueueCreate(10, sizeof(debugEvent));
+    vQueueAddToRegistry(syslog_q, "SYSLOG_Q");
 
     event_group = xEventGroupCreate();
+
+    xTaskCreate(buttonTask, "TASK1", 512, (void *) nullptr, tskIDLE_PRIORITY + 2, NULL);
+    xTaskCreate(debugTask, "TASK4", 512, (void *) nullptr, tskIDLE_PRIORITY + 1, NULL);
 
 
     vTaskStartScheduler();
